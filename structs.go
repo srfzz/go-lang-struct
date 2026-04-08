4fc4e871c4bf5734fd66a6b5c1dd1f16fb801b3d
Sarfaraj Ansari <imsarfaraaj7@gmail.com>
2026-04-08 21:12:39 +0530

    added type struct and initalized it with values

--- structs.go
+++ structs.go
@@ -1,12 +1,35 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
+
+type User struct {
+	firstName string
+	lastName  string
+	birthDate string
+	createdAt time.Time
+	updatedAt time.Time
+}
 
 func main() {
 	firstName := getUserData("Please Enter Your FirstName")
 	lastName := getUserData("Please Enter Your LastName")
 	birthDate := getUserData("Please Enter Your BirtDate (DD/MM/YY):")
 	fmt.Println(firstName, lastName, birthDate)
+	outputDetails(firstName, lastName, birthDate)
+	appUser = User{
+		firstName: firstName,
+		lastName:  lastName,
+		birthDate: birthDate,
+		createdAt: time.Now(),
+		updatedAt: time.Now().UTC(),
+	}
+}
+
+func outputDetails(firstName, lastname, birthDate string) {
+	fmt.Println(firstName, lastname, birthDate)
 }
 
 func getUserData(prompt string) string {
