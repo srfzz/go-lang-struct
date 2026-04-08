00ed31246dfaf84352c0edbb13faa1c9580bd9a2
Sarfaraj Ansari <imsarfaraaj7@gmail.com>
2026-04-08 21:23:23 +0530

    structs and pointers

--- structs.go
+++ structs.go
@@ -18,18 +18,21 @@ func main() {
 	lastName := getUserData("Please Enter Your LastName")
 	birthDate := getUserData("Please Enter Your BirtDate (DD/MM/YY):")
 	fmt.Println(firstName, lastName, birthDate)
-	outputDetails(firstName, lastName, birthDate)
+	var appUser User
 	appUser = User{
 		firstName: firstName,
 		lastName:  lastName,
 		birthDate: birthDate,
-		createdAt: time.Now(),
+		createdAt: time.Now().UTC(),
 		updatedAt: time.Now().UTC(),
 	}
+	outputDetails(&appUser)
+	fmt.Println(appUser.firstName, appUser.lastName, appUser.birthDate, appUser.createdAt, appUser.updatedAt)
 }
 
-func outputDetails(firstName, lastname, birthDate string) {
-	fmt.Println(firstName, lastname, birthDate)
+func outputDetails(u *User) {
+	u.firstName = "Sarfaraj ansari"
+	fmt.Println(u.firstName, u.lastName, u.birthDate, u.createdAt, u.updatedAt)
 }
 
 func getUserData(prompt string) string {
